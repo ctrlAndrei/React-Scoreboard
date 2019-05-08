8a60e847fd878889c1aade664c59ef1d1ece0295
ctrlAndrei <andrei.bucur550@gmail.com>
2019-05-08 13:03:14 +0300

    Updates

---
+++ precache-manifest.7ec23593eb9517b2e0eef2bf403639e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5eb2e4e84eddf3ba0c08",
+    "url": "/React-Scoreboard/static/js/runtime~main.5eb2e4e8.js"
+  },
+  {
+    "revision": "880adb4d9aee86424221",
+    "url": "/React-Scoreboard/static/js/main.880adb4d.chunk.js"
+  },
+  {
+    "revision": "daa0e87c1efdab0447cc",
+    "url": "/React-Scoreboard/static/js/1.daa0e87c.chunk.js"
+  },
+  {
+    "revision": "880adb4d9aee86424221",
+    "url": "/React-Scoreboard/static/css/main.74bddfc5.chunk.css"
+  },
+  {
+    "revision": "6ba172d673c724310e7b95457653c2f3",
+    "url": "/React-Scoreboard/index.html"
+  }
+];
